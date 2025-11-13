503ff223f9cb181c43a1f42f7de6983cd8ce2a37
GUNUK CHUNG <gun08190@naver.com>
2025-11-13 20:57:39 +0900

    Update PointsExchange.js

--- js/components/user/PointsExchange.js
+++ js/components/user/PointsExchange.js
@@ -4,24 +4,33 @@ const PointsExchange = {
         <div>
             <nav class="navbar">
                 <div class="container">
-                    <div class="navbar-brand">📚 독서 인증 플랫폼</div>
+                    <div class="navbar-brand" @click="$router.push('/dashboard')" style="cursor: pointer;">
+                        📚 독서 인증 플랫폼
+                    </div>
                     <div class="navbar-nav">
-                        <router-link to="/dashboard" class="nav-link">홈</router-link>
-                        <router-link to="/my-reviews" class="nav-link">내 감상문</router-link>
-                        <router-link to="/completed-quizzes" class="nav-link">내 퀴즈</router-link>
-                        <div class="dropdown">
-                            <a class="nav-link active">포인트 ▼</a>
+                        <router-link v-if="isLoggedIn" to="/my-reviews" class="nav-link">내 감상문</router-link>
+                        <router-link v-if="isLoggedIn" to="/completed-quizzes" class="nav-link">내 퀴즈</router-link>
+                        <div v-if="isLoggedIn" class="dropdown">
+                            <a class="nav-link">포인트 ▼</a>
                             <div class="dropdown-content">
                                 <router-link to="/points-exchange">포인트 교환소</router-link>
                                 <router-link to="/points-history">적립 내역</router-link>
                                 <router-link to="/points-requests">신청 내역</router-link>
                             </div>
                         </div>
-                        <router-link to="/my-page" class="nav-link">마이페이지</router-link>
-                        <a href="#" @click.prevent="logout" class="nav-link">로그아웃</a>
+                        <router-link v-if="isLoggedIn" to="/my-page" class="nav-link">마이페이지</router-link>
+                        <template v-if="isLoggedIn">
+                            <a href="#" @click.prevent="logout" class="nav-link">로그아웃</a>
+                        </template>
+                        <template v-else>
+                            <router-link to="/signup" class="nav-link">회원가입</router-link>
+                            <span style="color: #ddd; padding: 0 8px;">|</span>
+                            <router-link to="/login" class="nav-link">로그인</router-link>
+                        </template>
                     </div>
                 </div>
             </nav>
+
             <div class="container">
                 <div class="dashboard">
                     <h2>포인트 교환소 💎</h2>
